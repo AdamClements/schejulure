9c9987b1fec89a29e85517362fa911f3856ce4c6
AdamClements <adam@swiftkey.net>
2013-01-17 17:42:34 +0000

    Bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject schejulure "0.1.1"
+(defproject schejulure "0.1.2-SNAPSHOT"
   :description "A simple long-term scheduling library inspired by cron and future"
   :url "http://www.github.com/AdamClements/schejulure"
   :license {:name "Eclipse Public License"
