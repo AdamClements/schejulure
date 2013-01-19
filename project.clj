c1af23bb7599e681296f9dd4e132747c01b96274
Adam Clements <adam.clements@gmail.com>
2013-01-19 11:20:09 +0000

    Rearrange dependencies properly

--- project.clj
+++ project.clj
@@ -4,5 +4,6 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [midje "1.4.0"]
-                 [clj-time "0.4.4"]])
+                 [clj-time "0.4.4"]]
+  :profiles {:dev {:dependencies [[midje "1.4.0"]]
+                   :plugins [[lein-midje "2.0.1"]]}})
