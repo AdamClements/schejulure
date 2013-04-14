601494293f3a18d7d2febeb206c30f4e0241dc2b
Daniel Woelfel <dwwoelfel@gmail.com>
2013-04-14 05:03:09 -0700

    adds test for a Sunday

--- test/schejulure/core_test.clj
+++ test/schejulure/core_test.clj
@@ -9,7 +9,8 @@
 (facts "cron-of basically works"
       (cron-of (date-time 1986 10 23 16 30)) => [30 16 23 10 4]
       (cron-of (date-time 1986 10 22 8 30)) => [30 8 22 10 3]
-      (cron-of (date-time 2000 1 1 0 0)) => [0 0 1 1 6])
+      (cron-of (date-time 2000 1 1 0 0)) => [0 0 1 1 6]
+      (cron-of (date-time 2000 1 2 0 0)) => [0 0 2 1 0])
 
 (facts "cron-match can match crons"
        (cron-match? [0 0 2 3 4] [[0 1 2] [0 1 2] [0 1 2] [0 1 2] [0 1 2]]) => false
