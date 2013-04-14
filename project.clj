cf55b485289d34607ab217b093ad3e6a7d91a005
Adam Clements <adam.clements@gmail.com>
2013-04-14 22:52:47 +0100

    Bump midje to latest version

--- project.clj
+++ project.clj
@@ -5,5 +5,5 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [clj-time "0.4.4"]]
-  :profiles {:dev {:dependencies [[midje "1.4.0"]]
-                   :plugins [[lein-midje "2.0.1"]]}})
+  :profiles {:dev {:dependencies [[midje "1.5.1"]]
+                   :plugins [[lein-midje "3.0.1"]]}})
