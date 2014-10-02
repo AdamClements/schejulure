5c6d5d499142ab2277d5fc1f77161b5f70e0aa2d
Adam Clements <adam.clements@gmail.com>
2014-10-02 18:26:57 +0100

    Version bump

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject schejulure "0.1.5-SNAPSHOT"
+(defproject schejulure "1.0.1-SNAPSHOT"
   :description "A simple long-term scheduling library inspired by cron and future"
   :url "http://www.github.com/AdamClements/schejulure"
   :license {:name "Eclipse Public License"
